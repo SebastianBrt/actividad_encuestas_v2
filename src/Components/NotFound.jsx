1f2daaa81b4ec7856988689b8e3e35b5af5fa2c2
SebastianBrt <135865100+SebastianBrt@users.noreply.github.com>
2023-11-06 14:06:30 -0300

    god

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,11 @@
+
+
+
+const NotFound = () => (
+    <div>
+        <h2>Página no encontrada</h2>
+        <p>Lo sentimos, la página que estás buscando no existe.</p>
+    </div>
+);
+
+export default NotFound;
